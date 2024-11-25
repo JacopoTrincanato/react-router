073d7209130905ec64d8c8cf864f14e36a4163ba
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-25 15:50:15 +0100

    update homepage page

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,32 @@
+//creo il componente Homepage
+export default function Homepage() {
+    //eseguo il return
+    return (
+        <>
+
+            <section>
+                <h2>Benvenuto nel mio blog</h2>
+                <p>
+                    Scopri un mondo di idee, ispirazioni e approfondimenti. Che tu sia qui per curiosità, per imparare qualcosa di nuovo o per trovare il tuo momento di relax, sei nel posto giusto. Esplora i nostri post e lasciati ispirare!
+                </p>
+            </section>
+
+            <section>
+                <h2>Unisciti alla Nostra Community</h2>
+                <p>
+                    Non limitarti a leggere: partecipa! Condividi le tue idee, lascia un commento o seguici sui nostri canali social. Insieme, costruiamo uno spazio di confronto e ispirazione.
+                </p>
+            </section>
+
+            <section>
+                <h2>Contattaci</h2>
+                <p>
+                    Hai domande, suggerimenti o vuoi collaborare con noi? Scrivici! Saremo felici di ascoltarti.
+
+                    Indirizzo email: ilmioblog@gmail.com
+                </p>
+            </section>
+
+        </>
+    )
+}
