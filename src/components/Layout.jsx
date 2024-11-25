a976d446314b913519fdcd15a8f52a0419e240d5
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-25 15:27:17 +0100

    create Layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+//importo il componente Outlet
+import { Outlet } from "react-router-dom";
+//importo il componente AppHeader
+import AppHeader from "./AppHeader";
+//importo il componente AppFooter
+import AppFooter from "./AppFooter";
+
+//creo il componente Layout
+export default function Layout() {
+    //eseguo il return
+    return (
+        <>
+            <AppHeader />
+
+            {/*inserisco Outlet dentro main */}
+            <main>
+                <Outlet />
+            </main>
+
+            <AppFooter />
+        </>
+    )
+}
