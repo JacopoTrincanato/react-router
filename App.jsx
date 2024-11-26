b51fd3ed2dbc3e52d0720f52d1818fb06b7b5e62
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-26 12:55:35 +0100

    create CreaPost and export in in App.jsx

--- App.jsx
+++ App.jsx
@@ -15,6 +15,9 @@ import ChiSiamo from './src/Pages/ChiSiamo'
 //importo ListaPost
 import ListaPost from './src/Pages/ListaPost'
 
+//importo CreaPost
+import CreaPost from './src/Pages/CreaPost'
+
 function App() {
 
   return (
@@ -27,7 +30,9 @@ function App() {
           <Route element={<DefaultLayout />}>
             <Route path='/' element={<Homepage />}></Route>
             <Route path='/ChiSiamo' element={<ChiSiamo />}></Route>
-            <Route path='/ListaPost' element={<ListaPost />}></Route>
+            <Route path='/ListaPost' element={<ListaPost />}>
+              <Route path='create' element={<CreaPost />}></Route>
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/Pages/CreaPost.jsx
@@ -0,0 +1,11 @@
+import Form from "../components/form/Form";
+
+//creo il componente CreaPost
+export default function CreaPost() {
+
+    return (
+        <>
+            <Form />
+        </>
+    )
+}
