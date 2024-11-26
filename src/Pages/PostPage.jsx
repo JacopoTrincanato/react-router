f2c172b437dd08da402c0434cee084bf9c0a1f31
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-26 15:34:04 +0100

    add AJAX call to PostPage

--- src/Pages/PostPage.jsx
+++ src/Pages/PostPage.jsx
@@ -1,5 +1,11 @@
-import { useState } from "react";
+//importo useEffect e useState da react
+import { useEffect, useState } from "react";
+
+//importo useNavigate e useParams da react-router-dom
 import { useNavigate, useParams } from "react-router-dom";
+
+//importo Card
+import Card from "../components/card/Card";
 
 //creo il componente PostPage
 export default function PostPage() {
@@ -15,4 +21,30 @@ export default function PostPage() {
 
     //creo una costante che contenga l'url
     const url = `http://localhost:3002/posts/${slug}`
+
+    //fetcho i dati dall'endpoint dei post
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.data);
+
+            }).catch(err => {
+                console.log(err);
+
+            })
+    })
+
+    //eseguo il return
+    return (
+        <>
+            {
+                post ? (
+                    <Card />
+                ) : (
+                    <p>Il post non è presente</p>
+                )
+            }
+        </>
+    )
 }
